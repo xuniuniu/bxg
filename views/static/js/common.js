7242c2b8077c3a6acd91afce7069e2f2c65d190e
xiaoxu <xiaoxu.@xm.com>
2017-09-13 15:56:45 +0800

    退出功能完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -5,11 +5,28 @@
                  if(!$.cookie("PHPSESSID")){
                      location.href="/dashboard/login";
                  }
-             }
+             
              var userinfo=JSON.parse($.cookie("userinfo"));
              console.log(userinfo);
              var html=template("profile_tpl",userinfo);
              $("#profile").html(html);
+            
+            }
+
+            $("#logout_btn").click(function(){
+                $.ajax({
+                    url:"/api/logout",
+                    type:"post",
+                    success:function(data){
+                        console.log(data);
+                        if(data.code==200){
+                            location.href="/dashboard/login";
+                        }
+                    }
+
+                })
+            })
+
          })
     })
 
